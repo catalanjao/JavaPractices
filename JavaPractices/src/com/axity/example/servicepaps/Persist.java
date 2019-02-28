c05e2e09ad838141ed2e1e029e55c85a2a2336be
catalanjao <catalanjao@gmail.com>
2019-02-28 09:29:41 -0600

    delete

--- JavaPractices/src/com/axity/example/servicepaps/Persist.java
+++
@@ -1,9 +0,0 @@
-package com.axity.example.servicepaps;
-
-public abstract class Persist {
-    public abstract void save(String content);
-    public abstract void edit(long ifContent, String newContent);
-    public void delete(long idContent){
-
-    }
-}
--- JavaPractices/src/com/axity/example/services/IPersist.java
+++
@@ -1,7 +0,0 @@
-package com.axity.example.services;
-
-public interface IPersist {
-    void save(String content);
-    void delete(long idContent);
-    void edit(long ifContent, String newContent);
-}
